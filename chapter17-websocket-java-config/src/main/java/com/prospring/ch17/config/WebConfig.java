49b077e04979942cf450af99d75f1fea62bee790
IvanBelyaev <ivan.java.developer@gmail.com>
2023-07-12 00:50:58 +0300

    Spring WebSocket. Java-конфигурация

---
+++ chapter17-websocket-java-config/src/main/java/com/prospring/ch17/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.prospring.ch17.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.prospring.ch17")
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/static/index.html");
+    }
+}
